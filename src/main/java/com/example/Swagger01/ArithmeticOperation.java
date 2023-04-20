47600c6897d50c343fcc1ac5f6bd410d0570ed23
Giulia <giuliac35@gmail.com>
2023-04-20 14:37:02 +0200

    Removed Lombok. Created Constructor and getters in ArithmeticOperation

--- src/main/java/com/example/Swagger01/ArithmeticOperation.java
+++ src/main/java/com/example/Swagger01/ArithmeticOperation.java
@@ -1,18 +1,33 @@
 package com.example.Swagger01;
 
-import jdk.jfr.Description;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @EntityScan
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ArithmeticOperation {
-    private String name;
-    private int minNumberOfOperands;
-    private String description;
-    private String[] properties;
+    private final String name;
+    private final int minNumberOfOperands;
+    private final String description;
+    private final String[] properties;
+    public ArithmeticOperation(String name, int minNumberOfOperands, String description, String[] properties) {
+        this.name = name;
+        this.minNumberOfOperands = minNumberOfOperands;
+        this.description = description;
+        this.properties = properties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinNumberOfOperands() {
+        return minNumberOfOperands;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String[] getProperties() {
+        return properties;
+    }
 }
